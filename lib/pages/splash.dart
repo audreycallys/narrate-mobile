ba6282ab9e828a114f9e1c1ee7c068d29c0461c9
audreycallys <nevaely0107@gmail.com>
2026-09-13 20:07:47 +0700

    feat: menambahkan struktur awal splash dan main.dart

---
+++ lib/pages/splash.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:narrate_blog/constants/app_colors.dart';
+
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primary);
+  }
+}
