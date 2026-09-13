0e8285d9a6c305fc1e250ea1006e569fe36bc98c
audreycallys <nevaely0107@gmail.com>
2026-09-13 20:16:59 +0700

    feat: menambahkan logo & judul splash

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:narrate_blog/constants/app_colors.dart';
 
-
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.primary);
+      backgroundColor: AppColors.primary,
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(height: 70),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/images/logo/white.png', width: 96),
+
+                const SizedBox(width: 5),
+
+                const Text(
+                  "Narrate",
+                  style: TextStyle(
+                    fontFamily: 'PlayfairDisplay',
+                    fontSize: 30,
+                    fontWeight: FontWeight.w800,
+                    color: AppColors.white,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
