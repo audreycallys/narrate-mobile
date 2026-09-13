94da96576b84e6d67d76202cd7fde87d2115c761
audreycallys <nevaely0107@gmail.com>
2026-09-13 21:28:37 +0700

    tambahkan logo besar

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -14,6 +14,37 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: AppColors.primary,
+      body: SafeArea(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: 260,
+              child: Stack(
+                children: [
+                  Opacity(
+                    opacity: 0.12,
+                    child: ClipRect(
+                      child: Transform.scale(
+                        scale: 1.6,
+                        child: Image.asset(
+                          'assets/images/logo/white.png',
+                          width: double.infinity,
+                          height: double.infinity,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
