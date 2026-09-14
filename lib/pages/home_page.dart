54bb413819175dfeb00879b9a6eca8e112cb26b8
audreycallys <nevaely0107@gmail.com>
2026-09-14 15:44:14 +0700

    feat: tambahkan judul artikel populer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -34,151 +34,176 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: AppColors.white,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Halo, Sakezza Labiru!',
-                        style: TextStyle(
-                          fontFamily: 'PlusJakartaSans',
-                          fontSize: 24,
-                          fontWeight: FontWeight.w800,
-                          color: Colors.black,
-                        ),
-                      ),
-                      SizedBox(height: 3),
-                      Text(
-                        'Ada cerita apa hari ini?',
-                        style: TextStyle(
-                          fontFamily: 'PlusJakartaSans',
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ],
-                  ),
-
-                  CircleAvatar(radius: 24, backgroundColor: AppColors.primary),
-                ],
-              ),
-
-              const SizedBox(height: 20),
-
-              TextField(
-                decoration: InputDecoration(
-                  hintText: 'Cari artikel, kategori, atau topik...',
-                  hintStyle: const TextStyle(
-                    fontFamily: 'PlusJakartaSans',
-                    fontSize: 13,
-                    color: Color.fromARGB(255, 82, 82, 82),
-                  ),
-                  prefixIcon: const Icon(Icons.search, color: Colors.grey),
-                  filled: true,
-                  fillColor: const Color(0xFFECECEC),
-                  contentPadding: const EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 15,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(14),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-
-              const SizedBox(height: 20),
-
-              if (posts.isEmpty)
-                const SizedBox(
-                  height: 185,
-                  child: Center(child: CircularProgressIndicator()),
-                )
-              else
-                CarouselSlider(
-                  options: CarouselOptions(
-                    height: 185,
-                    viewportFraction: 1,
-                    autoPlay: true,
-                    enlargeCenterPage: false,
-                    onPageChanged: (index, reason) {
-                      setState(() {
-                        currentCarousel = index;
-                      });
-                    },
-                  ),
-                  items: posts.take(3).map((post) {
-                    return Container(
-                      width: double.infinity,
-                      margin: const EdgeInsets.symmetric(horizontal: 2),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(16),
-                        child: Stack(
-                          fit: StackFit.expand,
-                          children: [
-                            Image.network(post['imageUrl'], fit: BoxFit.cover),
-
-                            Container(
-                              decoration: const BoxDecoration(
-                                gradient: LinearGradient(
-                                  begin: Alignment.topCenter,
-                                  end: Alignment.bottomCenter,
-                                  colors: [Colors.transparent, Colors.black87],
-                                ),
-                              ),
-                            ),
-
-                            Positioned(
-                              left: 18,
-                              right: 18,
-                              bottom: 16,
-                              child: Text(
-                                post['title'],
-                                maxLines: 2,
-                                overflow: TextOverflow.ellipsis,
-                                style: const TextStyle(
-                                  fontFamily: 'PlusJakartaSans',
-                                  fontSize: 17,
-                                  fontWeight: FontWeight.w700,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  }).toList(),
-                ),
-
-              const SizedBox(height: 15),
-
-              if (posts.isNotEmpty)
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: List.generate(posts.take(3).length, (index) {
-                    return Container(
-                      width: 7,
-                      height: 7,
-                      margin: const EdgeInsets.symmetric(horizontal: 3),
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: currentCarousel == index
-                            ? AppColors.primary
-                            : Colors.grey.shade300,
-                      ),
-                    );
-                  }),
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Halo, Sakezza Labiru!',
+                          style: TextStyle(
+                            fontFamily: 'PlusJakartaSans',
+                            fontSize: 24,
+                            fontWeight: FontWeight.w800,
+                            color: Colors.black,
+                          ),
+                        ),
+                        SizedBox(height: 3),
+                        Text(
+                          'Ada cerita apa hari ini?',
+                          style: TextStyle(
+                            fontFamily: 'PlusJakartaSans',
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    CircleAvatar(
+                      radius: 24,
+                      backgroundColor: AppColors.primary,
+                    ),
+                  ],
                 ),
-            ],
+
+                const SizedBox(height: 20),
+
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Cari artikel, kategori, atau topik...',
+                    hintStyle: const TextStyle(
+                      fontFamily: 'PlusJakartaSans',
+                      fontSize: 13,
+                      color: Color.fromARGB(255, 82, 82, 82),
+                    ),
+                    prefixIcon: const Icon(Icons.search, color: Colors.grey),
+                    filled: true,
+                    fillColor: const Color(0xFFECECEC),
+                    contentPadding: const EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 20),
+
+                if (posts.isEmpty)
+                  const SizedBox(
+                    height: 185,
+                    child: Center(child: CircularProgressIndicator()),
+                  )
+                else
+                  CarouselSlider(
+                    options: CarouselOptions(
+                      height: 185,
+                      viewportFraction: 1,
+                      autoPlay: true,
+                      enlargeCenterPage: false,
+                      onPageChanged: (index, reason) {
+                        setState(() {
+                          currentCarousel = index;
+                        });
+                      },
+                    ),
+                    items: posts.take(3).map((post) {
+                      return Container(
+                        width: double.infinity,
+                        margin: const EdgeInsets.symmetric(horizontal: 2),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(16),
+                          child: Stack(
+                            fit: StackFit.expand,
+                            children: [
+                              Image.network(
+                                post['imageUrl'],
+                                fit: BoxFit.cover,
+                              ),
+
+                              Container(
+                                decoration: const BoxDecoration(
+                                  gradient: LinearGradient(
+                                    begin: Alignment.topCenter,
+                                    end: Alignment.bottomCenter,
+                                    colors: [
+                                      Colors.transparent,
+                                      Colors.black87,
+                                    ],
+                                  ),
+                                ),
+                              ),
+
+                              Positioned(
+                                left: 18,
+                                right: 18,
+                                bottom: 16,
+                                child: Text(
+                                  post['title'],
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: const TextStyle(
+                                    fontFamily: 'PlusJakartaSans',
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.w700,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+
+                const SizedBox(height: 15),
+
+                if (posts.isNotEmpty)
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: List.generate(posts.take(3).length, (index) {
+                      return Container(
+                        width: 7,
+                        height: 7,
+                        margin: const EdgeInsets.symmetric(horizontal: 3),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: currentCarousel == index
+                              ? AppColors.primary
+                              : Colors.grey.shade300,
+                        ),
+                      );
+                    }),
+                  ),
+
+                const SizedBox(height: 25),
+
+                const Text(
+                  'Artikel Terpopuler',
+                  style: TextStyle(
+                    fontFamily: 'PlusJakartaSans',
+                    fontSize: 18,
+                    fontWeight: FontWeight.w800,
+                    color: Colors.black,
+                  ),
+                ),
+
+                const SizedBox(height: 15),
+              ],
+            ),
           ),
         ),
       ),
